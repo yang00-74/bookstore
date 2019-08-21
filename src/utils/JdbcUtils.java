393ff9a0e992b81d0897cd961dd30b485b1f3007
yangye <1104149593@qq.com>
2019-08-21 22:48:12 +0800

    Format code

--- src/utils/JdbcUtils.java
+++ src/utils/JdbcUtils.java
@@ -8,64 +8,68 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class JdbcUtils {
-	
-	 private static ThreadLocal<Connection> tl=new ThreadLocal<Connection>();
-     private static DataSource ds;
-     static{
-    	 try{
-     		ds=new ComboPooledDataSource();
-     	}catch(Exception e){
-     		throw new ExceptionInInitializerError(e);
-     	}
-     }
-     public static DataSource getDataSource(){
-    	 return ds;
-     }
-     
-     public static Connection getConnection(){
-    	 	//»ñÈ¡°ó¶¨µ½Ïß³ÌµÄÁ´½Ó
-    		 try { 
-    			 Connection con=tl.get();  
-    			 if(con==null){
-				  con=ds.getConnection();
-				  con.setAutoCommit(false);
-    			}
-				tl.set(con);
-				return con;
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}    	 
-     }
-     public static void startTransaction(){  	
-    	 try { 
-    		 Connection con=getConnection();
-			 con.setAutoCommit(false);
-		 } catch (Exception e) {
-		    throw new RuntimeException(e);
-		  }
-     }
-     
-     public static void commitTranscation(){   	
-    	 try {
-    		 Connection con=getConnection();
-			if(con!=null){
-				con.commit();
-			}
-		 } catch (Exception e) {
-		    throw new RuntimeException(e);
-		  }
-     }
-     public static void closeCon(){
-    	Connection con=null; 
-    	try {
-    		 con=getConnection();
-			 if(con!=null){
-				con.close();
-			}
-		 } catch (Exception e) {
-		    throw new RuntimeException(e);
-		  }finally{
-				  tl.remove();
-		  }
-     }
+
+    private static ThreadLocal<Connection> tl = new ThreadLocal<Connection>();
+    private static DataSource ds;
+
+    static {
+        try {
+            ds = new ComboPooledDataSource();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
+    public static Connection getConnection() {
+        //ï¿½ï¿½È¡ï¿½ó¶¨µï¿½ï¿½ß³Ìµï¿½ï¿½ï¿½ï¿½ï¿½
+        try {
+            Connection con = tl.get();
+            if (con == null) {
+                con = ds.getConnection();
+                con.setAutoCommit(false);
+            }
+            tl.set(con);
+            return con;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void startTransaction() {
+        try {
+            Connection con = getConnection();
+            con.setAutoCommit(false);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void commitTranscation() {
+        try {
+            Connection con = getConnection();
+            if (con != null) {
+                con.commit();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeCon() {
+        Connection con = null;
+        try {
+            con = getConnection();
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            tl.remove();
+        }
+    }
 }
